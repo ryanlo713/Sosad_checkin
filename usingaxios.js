97d2cc48f176830a0c5b3125f1af7051d08e722e
Ryan Lo <12672523+ryanlo713@users.noreply.github.com>
2023-03-18 04:05:11 +0800

    Update usingaxios.js

--- usingaxios.js
+++ usingaxios.js
@@ -8,7 +8,7 @@ const sosad_session = tokens.sosad_session;
 const header = {
   headers: {
     Referer: "https://www.sosad.fun",
-    Host: "sosad.fun",
+    Host: "www.sosad.fun",
     "user-agent":
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
     cookie: `'${sosad_session}'`,
