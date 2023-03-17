90bf2a04039476ec9fb088f02dc0cc2c0993dfa4
Ryan Lo <12672523+ryanlo713@users.noreply.github.com>
2023-03-18 04:12:17 +0800

    Update token.js

--- helper/token.js
+++ helper/token.js
@@ -1,9 +1,9 @@
 const botToken = process.env.botToken;
 const chatID = process.env.chatID;
-const sosad_session = process.env.sosad_session;
+const sosad_cookie = process.env.sosad_cookie;
 
 module.exports = {
     botToken,
     chatID,
-    sosad_session
+    sosad_cookie
 }
